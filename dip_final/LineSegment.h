685c414aca637c5b34af6aa2eb94c2d7ed5826ad
MarvinChung <marvin852316497@gmail.com>
2019-05-28 21:15:29 +0800

    opencv required

--- dip_final/LineSegment.h
+++ dip_final/LineSegment.h
@@ -12,7 +12,28 @@ void RGBimage2binary(const cv::Mat& imageMat, cv::Mat& binaryMat, const size_t b
     //Apply thresholding
     cv::threshold(grayscaleMat, binaryMat, binaryThreshold, 255, cv::THRESH_BINARY);
 
+}
 
+void DrawLineSeg(const cv::Mat& imageMat, const int TL, const int TH, vector<cv::Vec4i>& lines)
+{
+    cv::Mat grayscaleMat(imageMat, cv2.COLOR_BGR2GRAY);
+	cv::Mat temp;
+	cv::GaussianBlur(imageMat, grayscaleMat, Size(3,3), 0, 0);
+	cv::Canny(imageMat, temp, TL, TH, 3);
+	cv::HoughLinesP(temp, lines, 1, 2 * cv::CV_PI/180, 100,1000, 50 );
+  	for( size_t i = 0; i < lines.size(); i++ )
+  	{
+    	cv::Vec4i l = lines[i];
+
+   		// oran = float(l[1] / col_size );
+    	double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
+
+    	if(angle  < 5  && angle >=-5   ){
+    		//if(1){
+        	cout << l[0] << "," << l[1] << "," << l[2] << "," << l[3] << endl;
+        	cv::line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+    	}
+  	}
 }
 
 
--- dip_final/main.cpp
+++ dip_final/main.cpp
@@ -30,3 +30,41 @@ int main (int argc, char **argv)
 
     cv::waitKey(0);
 }
+
+// int main (int argc, char **argv)
+// {
+
+//     if (argc != 2)
+//     {
+//             std::cout << "USE: " << argv[0] << " <file_path>" << std::endl;
+//             return 1;
+//     }
+
+//     //Our color image
+//     cv::Mat imageMat = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+//     if (imageMat.empty())
+//     {
+//             std::cerr << "ERROR: Could not read image " << argv[1] << std::endl;
+//             return 1;
+//     }
+
+//     //Grayscale matrix
+//     cv::Mat grayscaleMat (imageMat.size(), CV_8U);
+
+//     //Convert BGR to Gray
+//     cv::cvtColor( imageMat, grayscaleMat, CV_BGR2GRAY );
+
+//     //Binary image
+//     cv::Mat binaryMat(grayscaleMat.size(), grayscaleMat.type());
+
+//     //Apply thresholding
+//     cv::threshold(grayscaleMat, binaryMat, 100, 255, cv::THRESH_BINARY);
+
+//     //Show the results
+//     cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
+//     cv::imshow("Output", binaryMat);
+
+//     cv::waitKey(0);
+
+//     return 0;
+// }
