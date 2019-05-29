daa6e75f6821e7a3eb9358e3b671ac8b96de90a2
MarvinChung <marvin852316497@gmail.com>
2019-05-30 00:10:47 +0800

    can draw

--- LineSegment.h
+++ LineSegment.h
@@ -14,13 +14,52 @@ void RGBimage2binary(const cv::Mat& imageMat, cv::Mat& binaryMat, const size_t b
 
 }
 */
-void DrawLineSeg(const cv::Mat& imageMat, const int TL, const int TH, std::vector<cv::Vec4i>& lines)
+void DrawLineSeg(const cv::Mat& imageMat, const double TL, const double TH, std::vector<cv::Vec4i>& lines)
 {
-    cv::Mat grayscaleMat(imageMat, cv::COLOR_BGRA2GRAY);
-	cv::Mat temp;
-	cv::GaussianBlur(imageMat, grayscaleMat, cv::Size(3,3), 0, 0);
-	cv::Canny(imageMat, temp, TL, TH, 3);
-	cv::HoughLinesP(temp, lines, 1, 2 * CV_PI/180, 100,1000, 50 );
-  	}
+	//cv::Mat grayscaleMat (imageMat.size(), CV_8U);
+	//cv::Mat temp;
+	//cv::GaussianBlur(imageMat, grayscaleMat, cv::Size(3,3), 0, 0);
+	//cv::Canny(imageMat, temp, TL, TH, 3);
+	//cv::HoughLinesP(temp, lines, 1, 2 * CV_PI/180, 100,1000, 50 );
+	cv::Mat response;
+	const double CannyThres[] = {50, 200};
+	const int CannyAperture = 3;
+	const bool CannyL2Grad = true;
+	cv::Mat gray;
+	cv::cvtColor(imageMat, gray, cv::COLOR_BGR2GRAY);
+	cv::Canny( gray, response, CannyThres[0], CannyThres[1], CannyAperture, CannyL2Grad );
+	
+	cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Output", response);
+    cv::waitKey(0);
 
+	const double HoughDistRes = 1;
+	const double HoughAngRes = CV_PI/360;
+	const double HoughMinLen = 30;//30
+	const double HoughMaxGap = 50;//10
+	const int HoughThres = 30;//80
+
+	lines.clear();
+
+	cv::HoughLinesP( response, lines, HoughDistRes, HoughAngRes, HoughThres, HoughMinLen, HoughMaxGap );
+	for( size_t i = 0; i < lines.size(); i++ )
+  	{
+    	cv::Vec4i l = lines[i];
+
+   		// oran = float(l[1] / col_size );
+    	double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
+
+    	//if(angle  < 5  && angle >=-5   ){
+    		//if(1){
+        	std::cout << l[0] << "," << l[1] << "," << l[2] << "," << l[3] << std::endl;
+        	cv::line( imageMat, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 2, 8);
+    	//}
+  	}
+     
+	 cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
+     cv::imshow("Output", imageMat);
+     cv::waitKey(0);
+ 
+
+}
 
--- main.cpp
+++ main.cpp
@@ -9,36 +9,22 @@ int main (int argc, char **argv)
 	std::cout << "filename:" << argv[1] << std::endl;
     
 	
-	cv::Mat imageMat = cv::imread(argv[1], cv::CV_LOAD_IMAGE_COLOR );
+	cv::Mat imageMat = cv::imread(argv[1]);
     if (imageMat.empty())
     {
             std::cerr << "ERROR: Could not read image " << argv[1] << std::endl;
             return 1;
     }
     
-	
+	cv::resize(imageMat, imageMat, cv::Size(868, 512), 0, 0, CV_INTER_LANCZOS4);	
 
     //Binary image
     //cv::Mat binaryMat(imageMat.size(), CV_8U);
     
     //RGBimage2binary(imageMat, binaryMat, 100);
 	std::vector<cv::Vec4i> lines;
-	DrawLineSeg(imageMat, 50, 150, lines);
-    for( size_t i = 0; i < lines.size(); i++ )
-  	{
-    	cv::Vec4i l = lines[i];
-
-   		// oran = float(l[1] / col_size );
-    	double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
-
-    	if(angle  < 5  && angle >=-5   ){
-    		//if(1){
-        	std::cout << l[0] << "," << l[1] << "," << l[2] << "," << l[3] << std::endl;
-        	cv::line( imageMat, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3);
-    	}
-  	}
- 
-    //Show the results
+	DrawLineSeg(imageMat, 50, 200, lines);
+        //Show the results
     //cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
     //cv::imshow("Output", binaryMat);
 
